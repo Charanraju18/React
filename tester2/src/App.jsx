0e531c882e26db0ef0f76df6507f011cb77426ef
Charanraju18 <charanraju925@gmail.com>
2024-08-06 11:40:18 +0530

    commit

--- tester2/src/App.jsx
+++ tester2/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Demo,Demo1} from './component/classcomp'
-import Demo0 from './component/classcomp'
 
 
 function App() {
@@ -11,7 +9,7 @@ function App() {
 
   return (
     <>
-      {/* <div>
+      <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -30,10 +28,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p> */}
-
-      <div className="header"></div>
-
+      </p>
     </>
   )
 }
