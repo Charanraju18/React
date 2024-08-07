9418c41b6d8d58c698374d0943ac0fd8a9466a8b
Charanraju18 <charanraju925@gmail.com>
2024-08-07 10:16:33 +0530

    commit

--- tester2/src/App.jsx
+++ tester2/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import reactlogo from './assets/react.svg'
 
 
 function App() {
@@ -9,26 +10,24 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <div className="logo">
+          <img class="logo_img" src={reactlogo}></img>
+        </div>
+        <div className="nav_bar">
+          <ul className='nav'>
+            <a href="#"><li>Home</li></a>
+            <a href="#"><li>Dashboard</li></a>
+            <a href="#"><li>About us</li></a>
+            <a href="#"><li>Contact Us</li></a>
+          </ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="hero">
+        <div className="side_bar">SIDEBAR</div>
+        <div className="content">CONTENT</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="footer">FOOTER</div>
     </>
   )
 }
