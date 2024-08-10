878d775388b9fe4d4897e2496d3734e9ac085079
Charanraju18 <charanraju925@gmail.com>
2024-08-10 10:32:55 +0530

    commit

--- tester2/src/components/child.jsx
+++ tester2/src/components/child.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './child.css'
+// import luffy from '../assets/luffy.webp'
 function child(props) {
-	const {si,name,roll,clg} = props;
+	const {luffy,si,name,roll,clg,sd} = props;
+	console.log(sd)
   return (
 	<>
 		<div className='details'>
+			<img src={luffy} width='100px' height='83px'></img>
 			<h3>Student : {si}</h3>
 			<p>Name : {name}</p>
 			<p>Roll : {roll}</p>
 			<p>College : {clg}</p>
+			<p>Student Details : <br/>{sd.n},<br/>{sd.p},<br />{sd.lm}</p>
 		</div>
 	</>
   )
--- tester2/src/components/parent.jsx
+++ tester2/src/components/parent.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Child from "./child";
+import luffy from '../assets/luffy.webp'
 function parent() {
+  const stu_det = {n:"prabha", p:"actor", lm:"kalki"};
   return (
     <div>
-      <Child si="1" name="Sricharan" roll={4223} clg="ACOE" />
-      <Child si="2" name="charan" roll={4250} clg="ACOE" />
-      <Child si="3" name="praveen" roll={4220} clg="ACOE" />
+      <Child luffy={luffy} si="1" name="Sricharan" roll={4223} clg="ACOE" sd={stu_det}/>
+      <Child luffy={luffy} si="2" name="charan" roll={4250} clg="ACOE" sd={stu_det}/>
+      <Child luffy={luffy} si="3" name="praveen" roll={4220} clg="ACOE" sd={stu_det}/>
     </div>
   );
 }
