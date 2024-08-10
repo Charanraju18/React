f89b2c8d01c1f1db51cdad781066acdccc534d29
Charanraju18 <charanraju925@gmail.com>
2024-08-10 11:44:37 +0530

    commit

--- tester2/src/components/child.jsx
+++ tester2/src/components/child.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import './child.css'
+import "./parent";
 // import luffy from '../assets/luffy.webp'
 function child(props) {
-	const {luffy,si,name,roll,clg,sd} = props;
-	console.log(sd)
+  const { luffy, si, name, roll, clg, sd, func } = props;
+//   console.log(sd.n);
   return (
-	<>
-		<div className='details'>
-			<img src={luffy} width='100px' height='83px'></img>
-			<h3>Student : {si}</h3>
-			<p>Name : {name}</p>
-			<p>Roll : {roll}</p>
-			<p>College : {clg}</p>
-			<p>Student Details : <br/>{sd.n},<br/>{sd.p},<br />{sd.lm}</p>
-		</div>
-	</>
-  )
+    <>
+      <div className="details">
+        <img src={luffy} width="100px" height="83px"></img>
+        <h3>Student : {si}</h3>
+        <p>Name : {name}</p>
+        <p>Roll : {roll}</p>
+        <p>College : {clg}</p>
+        <p>
+          Student Details : <br />
+          {sd.n},<br />
+          {sd.p},<br />
+          {sd.lm}
+        </p>
+        {/* <button onClick={() => func("Praveen")}>Clcik me</button> */}
+        <button onClick={() => func(sd.n)}>Click Me</button>
+      </div>
+    </>
+  );
 }
 
 export default child
--- tester2/src/components/parent.jsx
+++ tester2/src/components/parent.jsx
@@ -1,13 +1,78 @@
 import React from "react";
 import Child from "./child";
 import luffy from '../assets/luffy.webp'
+// import { useState } from "react";
 function parent() {
-  const stu_det = {n:"prabha", p:"actor", lm:"kalki"};
+  // const [name, setName] = useState("Charan");
+  // console.log(name);
+  // function handleClick(param) {
+  //   setName(param);
+  // }
+  // console.log(name);
+
+  function fun(name){
+    console.log("hai "+name)
+  }
+
+  const stu_det = [
+    { n: "prabha", p: "actor", lm: "kalki" },
+    { n: "Rana", p: "actor", lm: "beemlanayak" },
+    { n: "NTR", p: "actor", lm: "RRR" },
+    { n: "Surya", p: "actor", lm: "Vikram" },
+    { n: "karthi", p: "actor", lm: "Japan"}
+  ];
   return (
-    <div>
-      <Child luffy={luffy} si="1" name="Sricharan" roll={4223} clg="ACOE" sd={stu_det}/>
-      <Child luffy={luffy} si="2" name="charan" roll={4250} clg="ACOE" sd={stu_det}/>
-      <Child luffy={luffy} si="3" name="praveen" roll={4220} clg="ACOE" sd={stu_det}/>
+    <div className="cards">
+      {/* {name} */}
+      <Child
+        luffy={luffy}
+        si="1"
+        name="Sricharan"
+        roll={4223}
+        clg="ACOE"
+        sd={stu_det[0]}
+        func={fun}
+      />
+      <Child
+        luffy={luffy}
+        si="2"
+        name="charan"
+        roll={4250}
+        clg="ACOE"
+        sd={stu_det[1]}
+        // fun={fun(stu_det[1].n)}
+        func={fun}
+      />
+      <Child
+        luffy={luffy}
+        si="3"
+        name="praveen"
+        roll={4220}
+        clg="ACOE"
+        sd={stu_det[2]}
+        // fun={fun(stu_det[2].n)}
+        func={fun}
+      />
+      <Child
+        luffy={luffy}
+        si="4"
+        name="Karthik"
+        roll={4254}
+        clg="ACOE"
+        sd={stu_det[3]}
+        // fun={fun(stu_det[3].n)}
+        func={fun}
+      />
+      <Child
+        luffy={luffy}
+        si="4"
+        name="Karthik"
+        roll={4254}
+        clg="ACOE"
+        sd={stu_det[4]}
+        // fun={fun(stu_det[3].n)}
+        func={fun}
+      />
     </div>
   );
 }
