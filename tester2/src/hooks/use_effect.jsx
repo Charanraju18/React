6d7e51228d1755ef9332a83f041a011d6a41ba04
Charanraju18 <charanraju925@gmail.com>
2024-08-18 21:09:49 +0530

    commit

--- tester2/src/hooks/use_effect.jsx
+++ tester2/src/hooks/use_effect.jsx
@@ -1,21 +1,47 @@
 import React, { useEffect, useState } from 'react'
 import './usestate.css'
+import img1 from '../assets/luffy.webp'
+import img2 from '../assets/netflix.jpg'
 
-function Use_effect() {
+function Use_effect0() {
 	const [count, setcount] = useState(0)
-	useEffect(()=>{
+	useEffect(() => {
 		setTimeout(() => {
-			setcount(count => count+1)
-		}, 1000);
+			setfirst(first-1)
+		}, 10);
+		if(first==0){
+			setfirst(100);
+		}
 	},[])
   return (
 	<>
 	<div className='div1'>
 		<h1>Use Effect</h1>
-		<h2>I have rendered {count} times</h2>
+		<h2>{count}</h2>
 	</div>
 	</>
   )
+}
+
+function Use_effect(){
+	const flag=false;
+	const [first, setfirst] = useState(img1);
+	const clicked=()=>{
+		if(flag){
+			setfirst(img1)
+		}
+		else{
+			setfirst(img2)
+		}
+		flag=!flag
+	}
+	return(
+		<>
+			<div className='image'>
+				<img src={}/>
+			</div>
+		</>
+	)
 }
 
 export default Use_effect
