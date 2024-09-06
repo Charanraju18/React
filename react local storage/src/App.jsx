9dbe7d2c1347e743b88fc618d91beb3dd9a5f20f
Charanraju18 <charanraju925@gmail.com>
2024-09-06 09:35:48 +0530

    commit

---
+++ react local storage/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Local from './Pages/Local'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Local/>
+    </div>
+  )
+}
+
+export default App
