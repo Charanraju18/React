f241ef6bc467a2f7194967a093037a038ccd12fb
Charanraju18 <charanraju925@gmail.com>
2024-09-19 11:09:38 +0530

    link tags commit 19-09

--- react local storage/src/Pages/About/About.jsx
+++ react local storage/src/Pages/About/About.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function About() {
   return (
 	<div>
 		<h1>About Page</h1>
+		<Link to="/">Home</Link><br />
+		<Link to="/contact">Contact</Link>
 	</div>
   )
 }
--- react local storage/src/Pages/Contact/Contact.jsx
+++ react local storage/src/Pages/Contact/Contact.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function Contact() {
   return (
 	<div>
 		<h1>Contact Page</h1>
+		<Link to="/">Home</Link><br />
+		<Link to="/about">About</Link>
 	</div>
   )
 }
